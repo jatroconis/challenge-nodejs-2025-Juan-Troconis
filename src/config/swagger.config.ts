6cf479ecdc8a284fb96925086b8f2dd32ca21dd7
Juan Troconis <jatroconis4@gmail.com>
2025-08-20 19:36:57 -0500

    [Config] add Swagger and Docker Compose To PGAdmin

---
+++ src/config/swagger.config.ts
@@ -0,0 +1,14 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export function setupSwagger(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('OlaClick Orders API')
+    .setDescription('API RESTful para gestionar órdenes de un restaurante')
+    .setVersion('1.0')
+    .addTag('orders')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { setupSwagger } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  setupSwagger(app);
+
+  await app.listen(process.env.APP_PORT || 3000);
 }
 bootstrap();
